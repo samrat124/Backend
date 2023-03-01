91ed5e0d521cf323a4ad28b9d33cdda212cd66cf
samrat kumar <sam9999950@gmail.com>
2023-03-01 20:25:21 +0530

    make server

---
+++ limit/index.js
@@ -0,0 +1,10 @@
+const express=require('express')
+const app=express();
+const port=3000;
+app.get('/',(res,req)=>{
+    res.send("Hello World")
+})
+
+app.listen(port,()=>{
+    console.log('server started')
+})
